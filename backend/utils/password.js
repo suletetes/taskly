f87cc803c8f28b4f7793ab5fb958e2b629ddac07
suleabdul <sulea841@gmail.com>
2025-10-19 15:14:01 +0100

    feat(utils): Add JWT and password utility functions

---
+++ backend/utils/password.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcryptjs');
+
+const SALT_ROUNDS = 12;
+
+/**
+ * Hash a password using bcrypt
+ * @param {string} password - Plain text password
+ * @returns {Promise<string>} Hashed password
+ */
+const hashPassword = async (password) => {
+  return await bcrypt.hash(password, SALT_ROUNDS);
+};
+
+/**
+ * Compare a plain text password with a hashed password
+ * @param {string} password - Plain text password
+ * @param {string} hashedPassword - Hashed password from database
+ * @returns {Promise<boolean>} True if passwords match
+ */
+const comparePassword = async (password, hashedPassword) => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
+module.exports = {
+  hashPassword,
+  comparePassword,
+};
